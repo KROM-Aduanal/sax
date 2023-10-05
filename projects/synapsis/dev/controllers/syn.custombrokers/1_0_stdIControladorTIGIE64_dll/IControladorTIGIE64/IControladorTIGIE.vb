b85da12ce7e9860e82bb87a89dac7f7702a8dc80
azamora287 <antonio.dominguez@kromaduanal.com>
2023-10-05 08:40:33 -0600

    Add IControladorTIGIE
    Se agrega interface del controlador de TIGIE

---
+++ projects/synapsis/dev/controllers/syn.custombrokers/1_0_stdIControladorTIGIE64_dll/IControladorTIGIE64/IControladorTIGIE.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text.RegularExpressions
+Imports System.Web.UI
+Imports gsol.krom
+Imports MongoDB.Bson
+Imports MongoDB.Bson.Serialization.Attributes
+Imports MongoDB.Driver
+Imports Rec.Globals.Utils
+Imports Syn.Documento
+Imports Syn.Documento.Componentes
+Imports Syn.Nucleo.RecursosComercioExterior
+Imports Syn.Operaciones
+Imports Wma.Exceptions
+Imports Syn.Nucleo.RecursosComercioExterior.CamposTarifaArancelaria
+
+Public Interface IControladorTIGIE
+
+#Region "Enums"
+
+    Enum TipoOperacion
+
+        Importacion = 1
+        Exportacion = 2
+
+    End Enum
+
+#End Region
+
+#Region "Propiedades"
+
+    Property Estado As TagWatcher
+
+#End Region
+
+#Region "Funciones"
+
+    Function EnlistarFracciones(ByVal texto_ As String) As TagWatcher
+    Function EnlistarNicosFraccion(ByVal fraccion_ As String) As TagWatcher
+    Function BuscarNico(ByVal id_ As ObjectId,
+                        Optional ByVal tipoOperacion_ As TipoOperacion = TipoOperacion.Importacion,
+                        Optional ByVal fecha_ As Date = Nothing,
+                        Optional ByVal pais_ As String = Nothing) As TagWatcher
+    Function TraeDatosFraccion(ByVal id_ As ObjectId,
+                               Optional ByVal tipoOperacion_ As TipoOperacion = TipoOperacion.Importacion,
+                               Optional ByVal pais_ As String = Nothing) As TagWatcher
+    Function TraeDatosFraccion(ByVal fraccion_ As String, Optional ByVal tipoOperacion_ As TipoOperacion = TipoOperacion.Importacion,
+                               Optional ByVal pais_ As String = Nothing) As TagWatcher
+    Function TraeDatosFraccion(Of T)(fraccion_ As String,
+                                      tipoOperacion_ As IControladorTIGIE.TipoOperacion,
+                                      pais_ As String,
+                                      fecha_ As Date) As TagWatcher
+
+
+#End Region
+
+End Interface
