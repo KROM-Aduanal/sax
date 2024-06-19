c512e796624e5d6e69fa921ce9883e93ff920bf1
azamora287 <antonio.dominguez@kromaduanal.com>
2024-06-19 11:03:29 -0600

    UP     - ConstructorReferencias NEW  - IControladorReferencias NEW  - ControladorReferencias UP     - Ges022-001-Referencias.apx UP     - Ges022-001-Referencias.apx.vb UP     - Ges022-001-TarifaArancelaria.apx UP     - Ges022-001-TarifaArancelaria.apx.vb NEW  - ControllerDocumentAnalyzer NEW  - IControllerDocumentAnalyzer NEW  - ControllerChatGPT NEW  - IControllerChatGPT UP     - Organismo2 UP     - RecursosComercioExterior UP     - Recursos

---
+++ masters/vb/esp/cmf/scr/Core/ia/pln/1_0_stdIControllerDocumentAnalyser64_dll/IControllerDocumentAnalyser64/IControllerDocumentAnalyzer.vb
@@ -0,0 +1,30 @@
+﻿Imports gsol.krom
+Imports Syn.Documento.Componentes
+Imports Syn.Nucleo.Recursos
+Imports Syn.Nucleo.RecursosComercioExterior
+Imports Syn.Documento.Componentes.Campo.TiposDato
+Imports MongoDB.Bson
+Imports System.IO
+Imports Wma.Exceptions
+
+Public Interface IControllerDocumentAnalyzer
+
+#Region "Enums"
+
+
+#End Region
+
+#Region "Properties"
+
+    Property Status As TagWatcher
+
+#End Region
+
+#Region "Funciones"
+
+    Function DocumentAnalyzer(Of T)(ByVal document_ As List(Of MemoryStream), Optional ByVal documetoCargado As IControllerChatGPT.DocumentoCargado = IControllerChatGPT.DocumentoCargado.BL) As TagWatcher
+    Function GetResponse(ByVal operationNumber_ As ObjectId) As TagWatcher
+
+#End Region
+
+End Interface
